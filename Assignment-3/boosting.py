86cc0c89e7476a0b1948c83f66e915557464a803
Xinyi Shen <xinyishe@usc.edu>
2018-03-17 17:45:23 -0700

    Problem 2 version 1.0

--- Assignment-3/boosting.py
+++ Assignment-3/boosting.py
@@ -24,9 +24,20 @@ class Boosting(Classifier):
 		return
 
 	def predict(self, features: List[List[float]]) -> List[int]:
-		########################################################
-		# TODO: implement "predict"
-		########################################################
+		N = len(features)
+		y_sum = np.zeros(N)
+		for t in range(0,self.T): # aggrate classifiers
+			y_t = self.clfs_picked[t].predict(features)
+			y_t = np.array(y_t)
+			y_sum += self.betas[t]*y_t
+		labels = []
+		for y in y_sum: # get labels
+			if y>0:
+				labels.append(1)
+			else:
+				labels.append(-1)
+		assert len(labels) == N
+		return labels
 		
 
 class AdaBoost(Boosting):
@@ -36,9 +47,42 @@ class AdaBoost(Boosting):
 		return
 		
 	def train(self, features: List[List[float]], labels: List[int]):
-		############################################################
-		# TODO: implement "train"
-		############################################################
+		N = len(labels)
+		labels =  np.array(labels)
+		#initialization w_0
+		w = np.array([1/N]*N)
+		indicator = np.zeros(N)
+		# print(features)
+		# print(labels)
+		for t in range(0,self.T):
+			#find h_t and compute eta_t
+			eta_t = 1
+			for clf in self.clfs:
+				labels_clf = np.array(clf.predict(features))
+				# print(labels_clf)
+				indicator_clf = np.heaviside(np.negative(np.multiply(labels,labels_clf)),0)
+				# print(indicator_clf)
+				error = np.dot(w,indicator_clf.T)
+				if error < eta_t:
+					eta_t =  error
+					h_t = clf
+					indicator = indicator_clf
+			#compute beta_t
+			beta_t =  0.5*np.log((1-eta_t)/eta_t)
+			#compute w
+			for n in range(0,N):
+				if indicator[n] == 0:
+					w[n] = w[n]*np.exp(-beta_t)
+				else:
+					w[n] = w[n]*np.exp(beta_t)
+			#normalize w
+			w_sum =  np.sum(w)
+			for n in range(0,N):
+				w[n] = w[n]/w_sum
+			#record h_t and beta_t
+			self.clfs_picked.append(h_t)
+			self.betas.append(beta_t)
+
 		
 		
 	def predict(self, features: List[List[float]]) -> List[int]:
@@ -52,9 +96,39 @@ class LogitBoost(Boosting):
 		return
 
 	def train(self, features: List[List[float]], labels: List[int]):
-		############################################################
-		# TODO: implement "train"
-		############################################################
+		N = len(labels)
+		labels = np.array(labels)
+		f = np.zeros(N)
+		# Initialization
+		pi =  np.array([0.5]*N)
+		h_tX = np.zeros(N)
+		for t in range(0,self.T):
+			#compute the working response
+			z = []
+			for n in range(0,N):
+				z.append(((labels[n]+1)/2-pi[n])/pi[n]*(1-pi[n]))
+			z = np.array(z)
+			#compute the weight
+			w = []
+			for n in range(0,N):
+				w.append(pi[n]*(1-pi[n]))
+			w = np.array(w)
+			#find h_t
+			error_min = -1
+			for clf in self.clfs:
+				labels_clf = np.array(clf.predict(features))
+				error = np.dot(w,np.square(z-labels_clf))
+				if error <= error_min or error_min == -1:
+					error_min = error
+					h_t = clf
+					h_tX = labels_clf
+			#update
+			f += 0.5*h_tX
+			#compute pi
+			for n in range(0,N):
+				pi[n] = 1/(1+np.exp(-2*f[n]))
+			self.clfs_picked.append(h_t)
+			self.betas.append(0.5)
 		
 		
 	def predict(self, features: List[List[float]]) -> List[int]:
--- Assignment-3/decision_stump.py
+++ Assignment-3/decision_stump.py
@@ -3,7 +3,7 @@ from typing import List
 from classifier import Classifier
 
 class DecisionStump(Classifier):
-	def __init__(self, s:int, b:float, d:float):
+	def __init__(self, s:int, b:float, d:int):
 		self.clf_name = "Decision_stump"
 		self.s = s
 		self.b = b
@@ -12,8 +12,12 @@ class DecisionStump(Classifier):
 	def train(self, features: List[List[float]], labels: List[int]):
 		pass
 		
-	def predict(self, features: List[float]) -> List[int]:
-		##################################################
-		# TODO: implement "predict"
-		##################################################
+	def predict(self, features: List[List[float]]) -> List[int]:
+		labels = []
+		for feature in features:
+			if feature[self.d] > self.b:
+				labels.append(self.s)
+			else:
+				labels.append(-self.s)
+		return labels
 		
